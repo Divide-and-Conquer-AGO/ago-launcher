e024bfb3192b8448506b844d74fd7fcfa356d5fc
Medik <steadyeddie101@hotmail.co.uk>
2025-06-02 13:47:52 +0100

    Add some tabs

--- src/gui/gui.go
+++ src/gui/gui.go
@@ -1,5 +1,51 @@
 package gui
 
-func InitGUI() {
+import (
+	"image/color"
 
+	"fyne.io/fyne"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+)
+
+func InitGUI() {
+	myApp := app.New()
+
+	myWindow := myApp.NewWindow("AGO Launcher")
+	myWindow.CenterOnScreen()
+	myWindow.FixedSize()
+	myWindow.Resize(fyne.NewSize(1500, 900))
+
+	RenderToolbar(myApp, myWindow)
+}
+
+func RenderToolbar(app fyne.App, mainWindow fyne.Window) {
+	tabs := container.NewAppTabs(
+		container.NewTabItemWithIcon("Home", theme.HomeIcon(), GetHomeContent()),
+		container.NewTabItemWithIcon("Settings", theme.SettingsIcon(), GetSettingsContent()),
+		container.NewTabItemWithIcon("About", theme.ComputerIcon(), GetAboutContent()),
+	)
+
+	tabs.SetTabLocation(container.TabLocationTop)
+	mainWindow.SetContent(tabs)
+	mainWindow.RequestFocus()
+	mainWindow.ShowAndRun()
+}
+
+func GetHomeContent() fyne.CanvasObject {
+	return widget.NewLabel("Home")
+}
+func GetSettingsContent() fyne.CanvasObject {
+	return widget.NewLabel("Settings")
+}
+func GetAboutContent() fyne.CanvasObject {
+	img := canvas.NewImageFromFile("icon.png")
+	img.FillMode = canvas.ImageFillOriginal
+	text := canvas.NewText("Overlay", color.Black)
+	content := container.New(layout.NewCenterLayout(), img, text)
+	return content
 }
--- src/main.go
+++ src/main.go
@@ -1,21 +1,9 @@
 package main
 
 import (
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
+	"ago-launcher/gui"
 )
 
 func main() {
-	a := app.NewWithID("AGO Launcher")
-	w := a.NewWindow("AGO Launcher")
-
-	hello := widget.NewLabel("Hello Fyne!")
-	w.SetContent(container.NewVBox(
-    		hello,
-    		widget.NewButton("Hi!", func() {
-    			hello.SetText("Welcome :)")
-    		}),
-    	))
-	w.ShowAndRun()
+	gui.InitGUI()
 }
